99eb0106d1e8c73fd781ee0fcded3073f6a75c50
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-04-05 19:19:10 +0200

    removed favourites section as it ref: favorites.html

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: 'index.html',                          
-                favorites: 'src/Favorites/favorites.html', 
-                movies: 'src/movies_selected/movies-selected.html' 
+                main: 'index.html',
+                favorites: 'src/Favorites/favorites.html',
+                movies: 'src/movies_selected/movies-selected.html'
             }
         }
     }
