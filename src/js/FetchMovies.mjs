cc02dc426613a1ee557f08d73160eb20367c7f3a
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-04-06 21:02:41 +0200

    Finished creating skeleton

--- src/js/FetchMovies.mjs
+++ src/js/FetchMovies.mjs
@@ -1,32 +1,15 @@
-import { displaySearchResults } from "./DisplayMovies.mjs";
+import { displaySearchResults, getGenreMap } from "./DisplayMovies.mjs";
+
 const tmdbKey = import.meta.env.VITE_TMDB_KEY;
-console.log("Injected env:", import.meta.env);
 
 export async function fetchMovies(query) {
     try {
-        const response = await fetch(
-            `https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}&query=${encodeURIComponent(query.toLowerCase())}`
-        );
-
-        if (!response.ok) {
-            document.querySelector("#movie-details").textContent =
-                "Failed to fetch movies. Please try again later.";
-            return [];
-        }
-
+        const genreMap = await getGenreMap();
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}&query=${encodeURIComponent(query)}`);
         const data = await response.json();
-
-        // TMDb returns results[], not Search
-        if (data.results && data.results.length > 0) {
-            displaySearchResults(data.results);
-        } else {
-            document.querySelector("#movie-details").innerHTML =
-                `<p>No results found for "<strong>${query}</strong>".</p>`;
-        }
+        displaySearchResults(data.results, genreMap);
 
     } catch (err) {
         console.error("Error fetching movies:", err);
-        document.querySelector("#movie-details").textContent =
-            "Something went wrong. Please try again.";
     }
 }
